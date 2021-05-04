8be2fc3132429f7392e0276aca7a2be51cc4ceb8
Thomas Binétruy <tbinetruy@gmail.com>
2021-05-04 12:31:10 +0200

    use my_dict.get(my_key, my_default) in FA1.2 contract

--- tests/end-to-end/FA12/contract.py
+++ tests/end-to-end/FA12/contract.py
@@ -28,9 +28,7 @@ class Contract:
     def approve(self, spender: address, value: int):
         allowance_key = AllowanceKey(SENDER, spender)
 
-        previous_value = 0
-        if allowance_key in self.allowances:
-            previous_value = self.allowances[allowance_key]
+        previous_value = self.allowances.get(allowance_key, 0)
 
         if previous_value > 0 and value > 0:
             raise Exception("UnsafeAllowanceChange")
@@ -40,26 +38,21 @@ class Contract:
     def transfer(self, _from: address, _to: address, value: int):
         if SENDER != _from:
             allowance_key = AllowanceKey(_from, SENDER)
-            authorized_value = 0
-            if allowance_key in self.allowances:
-                authorized_value = self.allowances[allowance_key]
+
+            authorized_value = self.allowances.get(allowance_key, 0)
 
             if (authorized_value - value) < 0:
                 raise Exception("NotEnoughAllowance")
 
             self.allowances[allowance_key] = authorized_value - value
 
-        from_balance = 0
-        if _from in self.tokens:
-            from_balance = self.tokens[_from]
+        from_balance = self.tokens.get(_from, 0)
 
         if from_balance - value < 0:
             raise Exception("NotEnoughBalance")
 
         self.tokens[_from] = from_balance - value
 
-        to_balance = 0
-        if _to in self.tokens:
-            to_balance = self.tokens[_to]
+        to_balance = self.tokens.get(_to, 0)
 
         self.tokens[_to] = to_balance + value
